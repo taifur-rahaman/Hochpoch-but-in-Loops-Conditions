71e78cda0665e8db52e1612368a1dae47bb9c88f
taifur-rahaman <nmlsDv@gmail.com>
2025-11-27 20:00:11 +0600

    Choices are given in main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+
+print("Hochpoch Problems")
+
+while True:
+    choice = input("Choose an option:\n1. Multiplication Table\n2. Digit Counter\n3. Sum of Digits\n4. Reverse Digits\n5. In Range finder\n0. Exit\nEnter Your Choice: ")
+    
+    if choice == "1":
+        pass # Multiplication Table Code goes here
+    elif choice == "2":
+        pass # Digit Counter Code goes here
+    elif choice == "3":
+        pass # Sum of Digits Code goes here
+    elif choice == "4":
+        pass # Reverse Digits Code goes here
+    elif choice == "5":
+        pass # In Range finder Code goes here
+    elif choice == "0":
+        print("Exiting the program. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.\n")
